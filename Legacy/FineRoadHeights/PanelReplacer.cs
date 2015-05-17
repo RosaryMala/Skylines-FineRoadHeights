9c104e2edc6e21b2e3907f97b5a3e8c847a1435c
SkylinesSimsfirehouse <hsuknowledge@gmail.com>
2015-05-17 20:49:33 +0800

    moved old files to Legacy

---
+++ Legacy/FineRoadHeights/PanelReplacer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace FineRoadHeights
+{
+    class PanelReplacer:MonoBehaviour
+    {
+        void Update()
+        {
+            //FineRoadHeightsLoadingExtension.ReplacePanels();
+            NetTool netTool = ToolsModifierControl.GetCurrentTool<NetTool>();
+            if (netTool == null)
+                return;
+            NetToolFine netToolFine = ToolsModifierControl.SetTool<NetToolFine>();
+            if (netToolFine == null)
+                return;
+            netToolFine.m_mode = netTool.m_mode;
+            netToolFine.m_placementCursor = netTool.m_placementCursor;
+            netToolFine.m_prefab = netTool.m_prefab;
+            netToolFine.m_snap = netTool.m_snap;
+            netToolFine.m_upgradeCursor = netTool.m_upgradeCursor;
+
+        }
+    }
+}
